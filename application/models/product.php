df8ff461dc9afd28a65d6bdbdef94da8209ebc81
Jimmy Jun <jimmy.s.jun@gmail.com>
2015-02-09 18:19:03 -0800

    updated Product model

--- application/models/product.php
+++ application/models/product.php
@@ -9,24 +9,20 @@ class Product extends CI_Model
 		$this->output->enable_profiler();
 	}
 
-}
-
-  class Product extends CI_Model
+  function get_all_products($page_num)
   {
-    function get_all_products($page_num)
-    {
-      $count = $this->db->query("SELECT count(id) AS num_products FROM products")->row_array();
-      $offset = 15 * ($page_num - 1);
-      $query = "SELECT products.id, products.name, price, url FROM products
-                LEFT JOIN categories on categories_id = categories.id
-                LEFT JOIN images ON products.id = images.product_id
-                LEFT JOIN image_types ON image_type_id = image_types.id
-                ORDER BY price ASC
-                LIMIT 15 OFFSET ?";
-      $all_products = array($this->db->query($query, $offset)->result_array(), $count['num_products']);
-      return $all_products;
-      // var_dump($all_products);
-      // die();
-    }
+    $count = $this->db->query("SELECT count(id) AS num_products FROM products")->row_array();
+    $offset = 15 * ($page_num - 1);
+    $query = "SELECT products.id, products.name, price, url FROM products
+              LEFT JOIN categories on categories_id = categories.id
+              LEFT JOIN images ON products.id = images.product_id
+              LEFT JOIN image_types ON image_type_id = image_types.id
+              ORDER BY price ASC
+              LIMIT 15 OFFSET ?";
+    $all_products = array($this->db->query($query, $offset)->result_array(), $count['num_products']);
+    return $all_products;
+    // var_dump($all_products);
+    // die();
   }
+}
 ?>
