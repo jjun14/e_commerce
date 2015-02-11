3467b59182aaf5d6c8eb1c8b4da61723c72aa407
Jimmy Jun <jimmy.s.jun@gmail.com>
2015-02-11 12:11:35 -0800

    updated db

--- updated.sql
+++ updated.sql
@@ -1,4 +1,6 @@
--- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (i386)
+CREATE DATABASE  IF NOT EXISTS `e_commerce` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `e_commerce`;
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
 --
 -- Host: 127.0.0.1    Database: e_commerce
 -- ------------------------------------------------------
@@ -37,7 +39,7 @@ CREATE TABLE `billings` (
   KEY `fk_billings_zipcodes1_idx` (`zipcode`),
   KEY `fk_billings_states1_idx` (`state`),
   KEY `fk_billings_cities1_idx` (`city`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,6 +48,7 @@ CREATE TABLE `billings` (
 
 LOCK TABLES `billings` WRITE;
 /*!40000 ALTER TABLE `billings` DISABLE KEYS */;
+INSERT INTO `billings` VALUES (12,'Jimmy','Jun','123 Dojo Way','Suite B','San Jose','CA','95112','2015-02-11 11:39:13',NULL);
 /*!40000 ALTER TABLE `billings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +67,7 @@ CREATE TABLE `carts` (
   PRIMARY KEY (`id`),
   KEY `fk_carts_users1_idx` (`user_id`),
   CONSTRAINT `fk_carts_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +76,7 @@ CREATE TABLE `carts` (
 
 LOCK TABLES `carts` WRITE;
 /*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+INSERT INTO `carts` VALUES (11,7,'2015-02-11 11:56:55','2015-02-11 11:56:55');
 /*!40000 ALTER TABLE `carts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +99,7 @@ CREATE TABLE `carts_have_products` (
   KEY `fk_carts_has_products_carts1_idx` (`cart_id`),
   CONSTRAINT `fk_carts_has_products_carts1` FOREIGN KEY (`cart_id`) REFERENCES `carts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_carts_has_products_products1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,6 +108,7 @@ CREATE TABLE `carts_have_products` (
 
 LOCK TABLES `carts_have_products` WRITE;
 /*!40000 ALTER TABLE `carts_have_products` DISABLE KEYS */;
+INSERT INTO `carts_have_products` VALUES (58,6,11,1,NULL,NULL),(59,1,11,3,NULL,NULL);
 /*!40000 ALTER TABLE `carts_have_products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +125,7 @@ CREATE TABLE `categories` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,6 +134,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Fashion',NULL,NULL),(2,'Music',NULL,NULL),(3,'Men\'s Interest',NULL,NULL),(4,'Sports',NULL,NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +151,7 @@ CREATE TABLE `image_types` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,6 +160,7 @@ CREATE TABLE `image_types` (
 
 LOCK TABLES `image_types` WRITE;
 /*!40000 ALTER TABLE `image_types` DISABLE KEYS */;
+INSERT INTO `image_types` VALUES (1,'main',NULL,NULL),(2,'secondary',NULL,NULL);
 /*!40000 ALTER TABLE `image_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +183,7 @@ CREATE TABLE `images` (
   KEY `fk_images_image_types1_idx` (`image_type_id`),
   CONSTRAINT `fk_images_image_types1` FOREIGN KEY (`image_type_id`) REFERENCES `image_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_images_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,6 +192,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'https://s3-us-west-1.amazonaws.com/cdecommerce/soccer.png',5,1,'0000-00-00 00:00:00',NULL),(2,'https://s3-us-west-1.amazonaws.com/cdecommerce/vibe.png',2,1,'0000-00-00 00:00:00','0000-00-00 00:00:00'),(3,'https://s3-us-west-1.amazonaws.com/cdecommerce/gq.png',1,1,'0000-00-00 00:00:00','0000-00-00 00:00:00'),(4,'https://s3-us-west-1.amazonaws.com/cdecommerce/mens_health.png',3,1,'0000-00-00 00:00:00','0000-00-00 00:00:00'),(5,'https://s3-us-west-1.amazonaws.com/cdecommerce/bazaar.png',4,1,'0000-00-00 00:00:00','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,7 +219,7 @@ CREATE TABLE `orders` (
   CONSTRAINT `fk_orders_billings1` FOREIGN KEY (`billing_id`) REFERENCES `billings` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_shippings1` FOREIGN KEY (`shipping_id`) REFERENCES `shippings` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_orders_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -273,7 +281,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `fk_products_categories1_idx1` (`category_id`),
   CONSTRAINT `fk_products_categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -282,6 +290,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'GQ','Gentlemen\'s Quarterly',13.99,55,1,NULL,NULL),(2,'Vibe','Vibe music magazine',11.99,67,2,NULL,NULL),(3,'Men\'s Health','Men\'s Health Magazine',14.99,14,3,NULL,NULL),(4,'Bazaar','Bazaar Fashion magazine',15.99,85,1,NULL,NULL),(5,'Soccer','Everything soccer',10.99,99,4,NULL,NULL);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -304,7 +313,7 @@ CREATE TABLE `shippings` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -313,6 +322,7 @@ CREATE TABLE `shippings` (
 
 LOCK TABLES `shippings` WRITE;
 /*!40000 ALTER TABLE `shippings` DISABLE KEYS */;
+INSERT INTO `shippings` VALUES (12,'Jimmy','Jun','123 Dojo Way','Suite B','San Jose','CA','95112','2015-02-11 11:38:53',NULL),(13,'Jimmy','Jun','123 Dojo Way',NULL,'San Jose','CA','95112','2015-02-11 11:54:15',NULL);
 /*!40000 ALTER TABLE `shippings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -329,7 +339,7 @@ CREATE TABLE `user_privileges` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -338,6 +348,7 @@ CREATE TABLE `user_privileges` (
 
 LOCK TABLES `user_privileges` WRITE;
 /*!40000 ALTER TABLE `user_privileges` DISABLE KEYS */;
+INSERT INTO `user_privileges` VALUES (1,'admin',NULL,NULL);
 /*!40000 ALTER TABLE `user_privileges` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -360,7 +371,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `fk_users_user_privileges1_idx` (`user_privilege_id`),
   CONSTRAINT `fk_users_user_privileges1` FOREIGN KEY (`user_privilege_id`) REFERENCES `user_privileges` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -369,6 +380,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (6,'Jimmy','Jun','jimmy@gmail.com','5f4dcc3b5aa765d61d8327deb882cf99',1,NULL,NULL),(7,NULL,NULL,NULL,NULL,NULL,'2015-02-11 11:56:55','2015-02-11 11:56:55');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -381,4 +393,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-02-11 11:22:23
+-- Dump completed on 2015-02-11 12:10:56
